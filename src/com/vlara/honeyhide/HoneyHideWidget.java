82cad291ee267795bd90aa5e8daa6774cab832de
Victor Lara <lara.m.victor@gmail.com>
2012-02-13 11:25:50 -0800

    Honey Hide V2.0

--- src/com/vlara/honeyhide/HoneyHideWidget.java
+++ src/com/vlara/honeyhide/HoneyHideWidget.java
@@ -7,80 +7,100 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.util.Log;
 import android.widget.RemoteViews;
 import android.widget.Toast;
 
 public class HoneyHideWidget extends AppWidgetProvider {
 
-	public static String ACTION_WIDGET_RECEIVER = "ActionReceiverWidget";
+    public static String ACTION_WIDGET_RECEIVER = "ActionReceiverWidget";
+    public static final String PREFS_NAME = "HoneyHideSettings";
+    public boolean hidden;
 
-	@Override
-	public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-		RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.main);
-		Intent Hide = new Intent(context, HoneyHideWidget.class);
-		Hide.setAction(ACTION_WIDGET_RECEIVER);
-		Hide.putExtra("msg", "Hid");
 
-		Intent Show = new Intent(context, HoneyHideWidget.class);
-		Show.setAction(ACTION_WIDGET_RECEIVER);
-		Show.putExtra("msg", "Show");
+    /*
+     * Reset hidden boolean Value to False whenever the widget is created(e.g on restart)
+     */
+    @Override
+    public void onEnabled(Context context) {
+        SharedPreferences settings = context.getSharedPreferences(PREFS_NAME, 0);
+        hidden = settings.getBoolean("hidden", false);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putBoolean("hidden", hidden);
+        editor.commit();
+    }
 
-		PendingIntent HidePendingIntent = PendingIntent.getBroadcast(context, 0, Hide, 0);
-		PendingIntent ShowPendingIntent = PendingIntent.getBroadcast(context, 1, Show, 1);
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        SharedPreferences settings = context.getSharedPreferences(PREFS_NAME, 0);
+        hidden = settings.getBoolean("hidden", false);
 
-		remoteViews.setOnClickPendingIntent(R.id.show, ShowPendingIntent);
-		remoteViews.setOnClickPendingIntent(R.id.hide, HidePendingIntent);
+        RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.main);
+        Intent toggle = new Intent(context, HoneyHideWidget.class);
+        toggle.setAction(ACTION_WIDGET_RECEIVER);
+        toggle.putExtra("msg", "toggle");
 
-		appWidgetManager.updateAppWidget(appWidgetIds, remoteViews);
-	}
+        PendingIntent TogglePendingIntent = PendingIntent.getBroadcast(context, 0, toggle, 0);
+        remoteViews.setOnClickPendingIntent(R.id.toggle,TogglePendingIntent);
+        appWidgetManager.updateAppWidget(appWidgetIds, remoteViews);
+    }
 
-	@Override
-	public void onReceive(Context context, Intent intent) {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        SharedPreferences settings = context.getSharedPreferences(PREFS_NAME, 0);
+        hidden = settings.getBoolean("hidden", false);
+        SharedPreferences.Editor editor = settings.edit();
+        if (intent.getAction().equals(ACTION_WIDGET_RECEIVER)) {
+            if (hidden) {
+                Toast.makeText(context, "Showing " + context.getString(R.string.SystemBar), Toast.LENGTH_SHORT).show();
+                Showbar();
+            }
+            else {
+                Toast.makeText(context, "Hiding " + context.getString(R.string.SystemBar), Toast.LENGTH_SHORT).show();
+                Hidebar();
+            }
+            editor.putBoolean("hidden", !hidden);
+            editor.commit();
+        }
 
-		if (intent.getAction().equals(ACTION_WIDGET_RECEIVER)) {
-			String msg = "null";
-			try {
-				msg = intent.getStringExtra("msg");
-			} catch (NullPointerException e) {
-				Log.e("Error", "msg = null");
-			}
-			Toast.makeText(context, msg + "ing " + context.getString(R.string.SystemBar), Toast.LENGTH_SHORT).show();
-			ToggleBar(msg);
-		}
+        super.onReceive(context, intent);
+    }
 
-		super.onReceive(context, intent);
-	}
+    public void Showbar() {
+        Thread t = new Thread(new Runnable() {
+            public void run() {
+                try {
+                    Log.d("OFF", "Turning ON");
+                    Process proc = Runtime.getRuntime().exec(new String[]{
+                            "am","startservice","-n","com.android.systemui/.SystemUIService"});
+                    proc.waitFor();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        t.start();
+    }
+    public void Hidebar(){
+        Thread t = new Thread(new Runnable() {
+            public void run() {
+                try {
+                    Log.d("OFF", "Turning OFF");
+                    Process proc = Runtime.getRuntime().exec(new String[]{
+                            "su","-c","service call activity 79 s16 com.android.systemui"});
+                    proc.waitFor();
+                    proc.destroy();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        t.start();
+    }
 
-	public void ToggleBar(String mode){
-
-		if (mode.equals("Hid")){
-			try {
-				Log.d("OFF", "Turning OFF");
-				Process proc = Runtime.getRuntime().exec(new String[]{
-						"su","-c","service call activity 79 s16 com.android.systemui"});
-				proc.waitFor();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-		else if(mode.equals("Show")){
-			try {
-				Log.d("OFF", "Turning ON");
-				Process proc = Runtime.getRuntime().exec(new String[]{
-						"am","startservice","-n","com.android.systemui/.SystemUIService"});
-				proc.waitFor();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-	}
 }
